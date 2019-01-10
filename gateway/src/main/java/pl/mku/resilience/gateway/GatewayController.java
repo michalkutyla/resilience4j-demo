c81b807b8a01e1c81ec6557d013baff07fe51e14
Michal <michal.kutyla.dev@gmail.com>
2019-01-10 12:48:46 +0100

    Reviews Service adds 5s latency and throw an exception. Gateway uses circuit braker to fail fast and give Reviews Service time to stabilise.

--- gateway/src/main/java/pl/mku/resilience/gateway/GatewayController.java
+++ gateway/src/main/java/pl/mku/resilience/gateway/GatewayController.java
@@ -1,5 +1,8 @@
 package pl.mku.resilience.gateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import java.time.Duration;
 import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,18 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class GatewayController {
 
+    private CircuitBreaker circuitBreaker = CircuitBreaker.of(
+        "Review Circuit Breaker",
+        CircuitBreakerConfig.custom()
+            .ringBufferSizeInClosedState(3)
+            .failureRateThreshold(100)
+            .waitDurationInOpenState(Duration.ofSeconds(30))
+            .ringBufferSizeInHalfOpenState(1)
+            .build());
+
     @RequestMapping(path = "/gateway/reviews")
     public List<String> reviews() {
-        return callReviewsService();
+        return circuitBreaker.executeSupplier(this::callReviewsService);
     }
 
     private List<String> callReviewsService() {
--- reviews-service/src/main/java/pl/mku/resilience/reviewsservice/ReviewsController.java
+++ reviews-service/src/main/java/pl/mku/resilience/reviewsservice/ReviewsController.java
@@ -2,6 +2,7 @@ package pl.mku.resilience.reviewsservice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewsController {
 
     @RequestMapping(path = "/reviews")
-    public List<String> details() {
-        return Arrays.asList(
-            "This product is great!",
-            "This product is worthless :(",
-            "I love this product :D");
+    public List<String> details() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
+        throw new RuntimeException();
+//        return Arrays.asList(
+//            "This product is great!",
+//            "This product is worthless :(",
+//            "I love this product :D");
     }
 
 }
